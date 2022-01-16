9c2bf03cb58cc0820faab8281e3c67c5c2a2dc1b
Tsundere Chen <me@tsunderechen.io>
2022-01-16 12:15:59 +0000

    Modify functions in utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
-def response_generate(result, source, source_text, responseMedia):
+def respond_decide(source_text):
+    return '回覆測試'
+
+def resp_json_gen(result, source, source_text, responseMedia):
     d = {}
     d['result'] = result
     d['source'] = source
